9a3778cd84d929ab1916a633ecb4585e7aeb3a16
jcostanza <jeremy.costanza@tritondigital.com>
2015-11-26 13:11:59 -0500

    Move output conditions to the last

--- src/main/scala/Bowling.scala
+++ src/main/scala/Bowling.scala
@@ -4,11 +4,11 @@ object Bowling {
 
   @tailrec
   def compute(scores: Seq[(Int)], acc: Int = 0): Int = scores match {
-    case Nil => acc
-    case s@Seq(10, s1, s2, t@_*) if t.isEmpty => acc + 10 + s1 + s2 // Handle Bonus Strike
+    case Seq(10, s1, s2, t@_*) if t.isEmpty => acc + 10 + s1 + s2 // Handle Bonus Strike
     case s@Seq(10, s1, s2, _*) => compute(s.tail, acc + 10 + s1 + s2) // Handle Strike
-    case s@Seq(a, b, c, t@_*) if a + b == 10 && t.isEmpty => acc + 10 + c // Handle Bonus Spare
+    case Seq(a, b, c, t@_*) if a + b == 10 && t.isEmpty => acc + 10 + c // Handle Bonus Spare
     case s@Seq(a, b, c, _*) if a + b == 10 => compute(s.drop(2), acc + 10 + c) // Handle Spare
     case s@Seq(a, b, _*) => compute(s.drop(2), acc + a + b) //Handle default
+    case Nil => acc
   }
 }
--- src/main/scala/BowlingWithSemiColon.scala
+++ src/main/scala/BowlingWithSemiColon.scala
@@ -5,12 +5,12 @@ object BowlingWithSemiColon {
 
   @tailrec
   def compute(scores: Seq[Int], acc: Int = 0): Int = scores match {
-    case Nil => acc
     case 10 :: s1 :: s2 :: Nil => acc + 10 + s1 + s2 // Handle Bonus Strike
     case 10 :: s1 :: s2 :: t => compute(s1 :: s2 :: t, acc + 10 + s1 + s2) // Handle Strike
     case a :: b :: c :: Nil if a + b == 10 => acc + 10 + c // Handle Bonus Spare
     case a :: b :: c :: t if a + b == 10 => compute(c :: t, acc + 10 + c) // Handle Spare
     case a :: b :: t => compute(t, acc + a + b) //Handle default
+    case Nil => acc
   }
 
 
